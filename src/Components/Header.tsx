e5dc55922c5fa57731317169b2cf610772131ce2
nastyashvec42 <nastyashvec42@gmail.com>
2023-02-03 19:27:49 +0300

    Lesson 12

---
+++ src/Components/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png'/>
+        </header>)
+}
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+            </div>
+            <div>
+                ava + discripcion
+            </div>
+            <div>
+                My Post
+                <div>
+                    New Post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>)
+}
