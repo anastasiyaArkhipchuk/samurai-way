528a59f96bc4f405658ef461f2be22aadc6d4896
nastyashvec42 <nastyashvec42@gmail.com>
2023-02-03 18:51:20 +0300

    Lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,53 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-        <header className='header'>
-            <img src='https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png'/>
-        </header>
-        <nav className='nav'>
-            <div>Profile</div>
-            <div>Message</div>
-        </nav>
-        <div className='content'>Main content</div>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://www.edigitalagency.com.au/wp-content/uploads/Twitter-logo-png.png'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img
+                        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+                </div>
+                <div>
+                    ava + discripcion
+                </div>
+                <div>
+                    My Post
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
