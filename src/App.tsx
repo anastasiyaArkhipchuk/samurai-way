85309d6f2c96ed67516bf96c3eb29388b7af9f0b
nastyashvec42 <nastyashvec42@gmail.com>
2023-03-07 21:53:46 +0300

    Lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import './App.css';
 import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
+import {Dialogs} from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 import {Profile} from "./Components/Profile/Profile";
+import {Settings} from "./Components/Settings/Settings";
+import {Music} from "./Components/Music/Music";
+import {News} from "./Components/News/News";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
+        <BrowserRouter>
+        <div>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Route path='/dialogs' component={Dialogs}/>
+                <Route path='/profile' component={Profile}/>
+                <Route path='/settings' component={Settings}/>
+                <Route path='/music' component={Music}/>
+                <Route path='/news' component={News}/>
+            </div>
         </div>
+        </BrowserRouter>
     );
 }
-
 export default App;
