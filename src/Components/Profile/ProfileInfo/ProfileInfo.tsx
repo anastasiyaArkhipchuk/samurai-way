98c0aa39b3ca123cae780a9d61fa7991b66a63bf
anastasiyaArkhipchuk <nastyashvec42@gmail.com>
2023-03-21 19:25:53 +0300

    image profile

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -7,7 +7,7 @@ export const ProfileInfo = () => {
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
             </div>
             <div className={s.descriptionBlock}>
-                ava + description
+                ava + descriptionb
             </div>
         </div>
     )
