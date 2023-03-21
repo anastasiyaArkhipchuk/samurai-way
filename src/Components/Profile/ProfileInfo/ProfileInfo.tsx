eb38e40caf5c6fcfc69594cc6d912e6571a7a482
NastyaArkh <NastyaArkh@apples-MacBook-Air.local>
2023-03-21 19:23:16 +0300

    image profile

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,7 +3,7 @@ import s from './ProfileInfo.module.css'
 export const ProfileInfo = () => {
     return (<div>
             <div>
-                <img
+                <img className={s.imageFon}
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
             </div>
             <div className={s.descriptionBlock}>
